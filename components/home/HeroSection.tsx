0e9252c524483fdc6662951fbf76e68cc3392a43
Maria Zavala <zaval054@umn.edu>
2025-03-20 14:30:17 -0700

    Changed Hero Text Size

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -23,11 +23,11 @@ export default function HeroSection({ src, alt }: Readonly<HeroSectionProps>) {
 
       {/* Content container */}
       <div className='relative flex flex-col items-center z-10 container mx-auto max-w-4xl text-center text-white px-6'>
-        <h1 className='max-w-2xl text-[clamp(4rem,5vw,5rem)] sm:text-7xl font-montserrat tracking-wider font-black leading-tight mb-2'>
+        <h1 className='max-w-2xl text-6xl sm:text-7xl font-montserrat tracking-wider font-black leading-tight mb-2'>
           IZZYS
         </h1>
         {/* Headline */}
-        <h1 className='max-w-2xl text-5xl sm:text-6xl font-mono font-black leading-tight mb-20'>
+        <h1 className='max-w-2xl text-4xl sm:text-6xl font-mono font-black leading-tight mb-20'>
           Landscaping & Construction
         </h1>
 
@@ -38,7 +38,7 @@ export default function HeroSection({ src, alt }: Readonly<HeroSectionProps>) {
         />
 
         {/* Subheading */}
-        <h3 className='text-lg sm:text-xl md:text-4xl mt-14 font-normal'>
+        <h3 className='text-xl sm:text-xl md:text-4xl mt-14'>
           Trusted Minnesota Landscaper For 20+ Years
         </h3>
       </div>
