20f12b79b884ead24a873ca12d4e8fdbd1cfc279
Maria Zavala <zaval054@umn.edu>
2025-03-21 00:57:16 -0700

    Unecessary css

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -10,7 +10,8 @@ interface HeroSectionProps {
 
 export default function HeroSection({ src, alt }: Readonly<HeroSectionProps>) {
   return (
-    <section className='w-full min-h-[65vh] md:min-h-[70vh] xl:min-h-[80vh] h-screen flex items-center justify-center py-16 px-4 bg-cover bg-center relative'>
+    // min-h-[65vh] md:min-h-[70vh] xl:min-h-[80vh]
+    <section className='w-full h-screen flex items-center justify-center py-16 px-4 bg-cover bg-center relative'>
       <Image
         src={src}
         alt={alt ?? "hero section"}
